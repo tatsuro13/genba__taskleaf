f767c50ab1671573e60c5a772c4c4b299238530f
tatsuro13 <oscarwild13@gmail.com>
2019-09-16 23:07:47 +0900

    ログイン機構手前

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,9 @@
 class Task < ApplicationRecord
+    validates :name, presence: true, length: {maximum: 30}
+    # validates :validate_name_not_including_comma
+
+    private
+      def validate_name_not_including_comma
+        errors.add(:name, 'にカンマを含めることはできません') if name&.include_(',')
+      end
 end
