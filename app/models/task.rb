b0032f30762ff835e5a293848b983959dc90f5f2
tatsuro13 <oscarwild13@gmail.com>
2019-09-23 12:00:56 +0900

    chapter４のタスク一覧CRUDの完成（テストなし）

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,9 @@
 class Task < ApplicationRecord
     validates :name, presence: true, length: {maximum: 30}
     # validates :validate_name_not_including_comma
+    belongs_to :user
+
+    scope :recent, -> {order(created_at: :desc)}
 
     private
       def validate_name_not_including_comma
