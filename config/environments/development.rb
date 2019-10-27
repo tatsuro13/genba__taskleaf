ad3035e47d689bdb209d9a6bcabf0ebae97be766
tatsuro osuka <oscarwild13@gmail.com>
2019-10-27 11:32:15 +0900

    303page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
+  config.log_level = :warn
+
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
@@ -51,6 +53,8 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
+
+  config.logger = Logger.new('log/development.log', 'daily')
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
