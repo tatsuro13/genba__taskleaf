5fc509a51d35aa7a7c88a2d7bf1407c80dd3c81f
tatsuro13 <oscarwild13@gmail.com>
2020-01-01 11:18:58 +0900

    358page ajaxによる削除操作実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -54,7 +54,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    # head :no_content
+    # redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
 
